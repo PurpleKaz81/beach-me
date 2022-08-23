38262015b73ab40015b2c52baf1e48ae99d7fe42
Rafael Kasinski <rakasin@gmail.com>
2022-08-23 13:37:44 -0300

    Finished several controller methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,40 @@
 class ProductsController < ApplicationController
-  # def index
+  def index
+    @products = Product.all
+  end
 
-  # end
+  def show
+    @product = Product.find(params[:id])
+  end
 
-  # def show
+  def new
+    @product = Product.new
+  end
 
-  # end
+  def create
+    @product = Product.new(params)
+    if @product.save
+      redirect_to @product, notice: "Product created successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+
+  end
+
+  def uodate
+
+  end
+
+  def destroy
+
+  end
+end
+
+private
+
+def params
+  params.require(:product).permit(:name, :description, :price, :available?)
 end
