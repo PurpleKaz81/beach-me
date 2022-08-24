3cce8a61a702e2e909151cd473d50c2b8d047e1d
Rafael Kasinski <rakasin@gmail.com>
2022-08-24 16:05:56 -0300

    Finished logout logic and rent logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
   end
+
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "pages#home"
+  devise_for :users, controllers: {sessions: "sessions"}
   resources :products do
     resources :rents, only: %i[create]
     get "rented", on: :collection
     get "mine", on: :collection
   end
   resources :products, only: :destroy
+  root to: "pages#home"
 end
