8e7bb6edcc483ba12099f6c3325aa38c5d10f043
Rafael Kasinski <rakasin@gmail.com>
2022-08-25 13:46:32 -0300

    Added date column to Rents

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_150644) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_164445) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_150644) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity"
+    t.date "date"
     t.index ["product_id"], name: "index_rents_on_product_id"
     t.index ["user_id"], name: "index_rents_on_user_id"
   end
