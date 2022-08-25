202d98fa0f836f59862b0a95ce11ac452d521c16
Rafael Kasinski <rakasin@gmail.com>
2022-08-25 11:36:06 -0300

    Fixed rent logic, missing quantity logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_170515) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_135925) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_170515) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
