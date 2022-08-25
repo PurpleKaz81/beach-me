c71b0fcb6319591b8a8b186627df6f1cb74f26cd
Rafael Kasinski <rakasin@gmail.com>
2022-08-25 15:32:17 -0300

    Finished rent date logic

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -3,5 +3,5 @@ class Rent < ApplicationRecord
   belongs_to :product, dependent: :destroy
   validates :quantity, presence: true, numericality: { greater_than: 0 }
   validates :rent_date, :return_date, presence: true
-  validates :end_date, comparison: { greater_than_or_equal_to: :start_date }
+  validates :return_date, comparison: { greater_than_or_equal_to: :rent_date }
 end
