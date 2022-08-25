d8399997fe61bc527a0f7a8defa831f6fda25044
Gabriel Teixeira Soares <gabriel_soares10@hotmail.com>
2022-08-25 18:23:28 -0300

    ultimoteste

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_180609) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity"
+    t.date "date"
     t.date "rent_date"
     t.date "return_date"
     t.index ["product_id"], name: "index_rents_on_product_id"
