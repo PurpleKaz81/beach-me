c1b7696a813582bba4b34996d92a0d6bd7465caf
Gabriel Teixeira Soares <gabriel_soares10@hotmail.com>
2022-08-26 12:23:13 -0300

    delete

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,7 +1,22 @@
 class Rent < ApplicationRecord
   belongs_to :user
   belongs_to :product, dependent: :destroy
-  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: {greater_than: 0}
   validates :rent_date, :return_date, presence: true
   validates :return_date, comparison: { greater_than_or_equal_to: :rent_date }
+  after_validation :decrease_product_quantity
+
+  private
+  
+  def decrease_product_quantity
+    return if self.quantity.blank?
+    
+    if self.quantity <= self.product.quantity
+    self.product.quantity -= self.quantity
+    self.product.save
+    else
+    self.errors.add(:quantity, "is greater than available quantity")
+    end
+  end
+  
 end
